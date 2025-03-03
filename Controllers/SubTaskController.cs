fd1073c166ed246b82a6f69cf4d5a2e965768172
TungNC-HS170239 <tungnchs170239@fpt.edu.vn>
2025-03-03 11:02:10 +0700

    create subtask
    create subtask

---
+++ Controllers/SubTaskController.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Planify_BackEnd.DTOs.SubTasks;
+using Planify_BackEnd.Services.SubTasks;
+
+namespace Planify_BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubTaskController : ControllerBase
+    {
+        private readonly ISubTaskService _subTaskService;
+        public SubTaskController(ISubTaskService subTaskService)
+        {
+            _subTaskService = subTaskService;
+        }
+        [HttpPost("create")]
+        [Authorize(Roles = "Implementer")]
+        public async Task<IActionResult> CreateSubTask([FromBody] SubTaskCreateRequestDTO subTaskDTO)
+        {
+            var implementerId = Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+            var response = await _subTaskService.CreateSubTaskAsync(subTaskDTO, implementerId);
+            return StatusCode(response.Status, response);
+        }
+    }
+}
