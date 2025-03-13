7986e619ec1e8113dcb403a11d8d194a929a2123
Thu <thunvhe176252@fpt.edu.vn>
2025-03-13 23:00:41 +0700

    fix connect to database

--- Models/PlanifyContext.cs
+++ Models/PlanifyContext.cs
@@ -59,10 +59,6 @@ public partial class PlanifyContext : DbContext
 
     public virtual DbSet<Ward> Wards { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=Planify;TrustServerCertificate=True;Trusted_Connection=True;User ID=sa;Password=123");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Address>(entity =>
