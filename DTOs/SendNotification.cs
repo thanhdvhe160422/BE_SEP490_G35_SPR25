b11e9b60384e9cf12357a23dd4f837c669babb06
Thu <thunvhe176252@fpt.edu.vn>
2025-04-02 22:27:04 +0700

    update api event for spec add authorize

---
+++ DTOs/SendNotification.cs
@@ -0,0 +1,9 @@
+﻿namespace Planify_BackEnd.DTOs
+{
+    public class SendNotification
+    {
+        Guid UserId { get; set; }
+        string Email { get; set; }
+        string Message { get; set; }
+    }
+}
