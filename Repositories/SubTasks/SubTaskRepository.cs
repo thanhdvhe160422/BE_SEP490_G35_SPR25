d9a64dc88ac2ffc5512000788a64fa8d5cbaf1dd
Thu <thunvhe176252@fpt.edu.vn>
2025-04-05 16:47:58 +0700

    update api search subtask compare status

--- Repositories/SubTasks/SubTaskRepository.cs
+++ Repositories/SubTasks/SubTaskRepository.cs
@@ -225,7 +225,7 @@ public class SubTaskRepository : ISubTaskRepository
                    .Where(jt => jt.UserId == implementerId &&
                                jt.Task.StartTime <= endDate &&
                                (jt.Task.Deadline >= startDate || jt.Task.Deadline == null) &&
-                               jt.Task.Status == 1)
+                               jt.Task.Status != -1)
                    .Select(jt => jt.Task)
                 .OrderBy(e => e.StartTime)
                 .Count();
@@ -234,7 +234,7 @@ public class SubTaskRepository : ISubTaskRepository
                    .Where(jt => jt.UserId == implementerId && 
                                jt.Task.StartTime <= endDate &&
                                (jt.Task.Deadline >= startDate || jt.Task.Deadline == null) &&
-                               jt.Task.Status == 1)
+                               jt.Task.Status != -1)
                 .OrderBy(e => e.Task.StartTime)
                 .Select(jt => new SubTask
                 {
