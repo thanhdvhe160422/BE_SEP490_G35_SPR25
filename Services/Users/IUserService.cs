f5e9b617c92a3b8a159777518b345049c0dcc635
Thu <thunvhe176252@fpt.edu.vn>
2025-04-09 12:33:17 +0700

    update api search user fix return value

--- Services/Users/IUserService.cs
+++ Services/Users/IUserService.cs
@@ -9,7 +9,7 @@ namespace Planify_BackEnd.Services.Users
         Task<UserDetailDTO> GetUserDetailAsync(Guid id);
         Task<ResponseDTO> UpdateUserStatusAsync(Guid id, int newStatus);
         PageResultDTO<UserListDTO> GetListImplementer(int eventId, int page, int pageSize);
-        Task<List<Models.User>> GetUserByNameOrEmailAsync(string input, int campusId);
+        Task<PageResultDTO<UserListDTO>> GetUserByNameOrEmailAsync(int page, int pageSize, string input, int campusId);
         Task<UserListDTO> CreateEventOrganizer(UserDTO userDTO);
         Task<UserListDTO> UpdateEventOrganizer(UserDTO userDTO);
         Task<UserRoleDTO> AddUserRole(UserRoleDTO roleDTO);
