287db4eb256237ef77b427737f8bb74e6144426e
Thu <thunvhe176252@fpt.edu.vn>
2025-04-21 14:31:42 +0700

    api crud campus

--- Repositories/Campus/ICampusRepository.cs
+++ Repositories/Campus/ICampusRepository.cs
@@ -4,5 +4,10 @@ public interface ICampusRepository
     {
          Task<IEnumerable<Campus>> getAllCampus();
         Task<Campus> GetCampusByName(string campusName);
-    }
+        Task<Campus?> GetCampusById(int id);
+        Task<bool> CreateCampus(Campus campus);
+        Task<bool> UpdateCampus(Campus campus);
+        Task<bool> DeleteCampus(int id);
+
+}
 
